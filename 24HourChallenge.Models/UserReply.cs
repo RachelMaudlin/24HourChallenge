58398fb6fa518ca47e5ba5c93a725e61d5ab62ed
RachelMaudlin <73568853+RachelMaudlin@users.noreply.github.com>
2021-02-06 14:51:56 -0500

    added stash to shanes change

--- 24HourChallenge.Models/UserReply.cs
+++ 24HourChallenge.Models/UserReply.cs
@@ -8,7 +8,12 @@ namespace _24HourChallenge.Models
 {
     public class UserReply
     {
+<<<<<<< Updated upstream
         public int Id { get; set; }
+=======
+
+        public string Text { get; set; }
+>>>>>>> Stashed changes
         //either do this as a class like you've started or...
         //have it inherit from the comment class.
         //If enough time then try option 2 for practice.
