51bfd21afaf852093be1253a709146e5f454ba01
RachelMaudlin <73568853+RachelMaudlin@users.noreply.github.com>
2021-02-06 11:53:49 -0500

    created services class

---
+++ 24HourChallenge.Services/PostService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24HourChallenge.Services
+{
+    public class PostService
+    {
+        private readonly Guid _userID; 
+
+        public PostService(Guid userID)
+        {
+            _userID = userID;
+        }
+
+    
+       
+    }
+}
