cb4c245e0c0d667f9d6bdf9d5398269aa5dbf051
shane86920915 <73564809+shane86920915@users.noreply.github.com>
2021-02-06 11:52:05 -0500

    add annotation

--- 24HourChallenge.Data/Post.cs
+++ 24HourChallenge.Data/Post.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,16 +9,23 @@ namespace _24HourChallenge.Data
 {
     class Post
     {
+        [Key]
         public Guid OwnerId { get; set; }
+        [Required]
         public int PostId { get; set; }
+        [Required]
         public string Title { get; set; }
+        [Required]
         public string  PostText { get; set; }
+        [Required]
         public int CommentId { get; set; }
+        [Required]
         public string CommentText { get; set; }
-
+        [Required]
         public DateTimeOffset CreatedUtc { get; set; }
 
         public DateTimeOffset? ModifiedUtc { get; set; }
+
 
     }
 }
