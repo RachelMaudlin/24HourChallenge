1d76f266640aafa1c094284e3c0e0f152192ec9b
shane86920915 <73564809+shane86920915@users.noreply.github.com>
2021-02-06 10:19:23 -0500

    testing

---
+++ 24HourChallenge.Data/Post.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24HourChallenge.Data
+{
+    class Post
+    {
+        public Guid OwnerId { get; set; }
+        public int PostId { get; set; }
+        public string Title { get; set; }
+        public string  PostText { get; set; }
+        public int CommentId { get; set; }
+        public string CommentText { get; set; }
+
+        public DateTimeOffset CreatedUtc { get; set; }
+
+        public DateTimeOffset? ModifiedUtc { get; set; }
+
+    }
+}
